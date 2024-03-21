94b9a0638ec4ea51c21f9c559c4118cabf2a4488
cagrisayir <omercagris@gmail.com>
2024-03-21 14:28:02 +0300

    added qr code

--- QRCode Service/task/src/qrcodeapi/controller/AppController.java
+++ QRCode Service/task/src/qrcodeapi/controller/AppController.java
@@ -26,16 +26,17 @@ public class AppController {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<?> getImage(@RequestParam int size,
+    public ResponseEntity<?> getImage(@RequestParam String contents,
+                                      @RequestParam int size,
                                       @RequestParam String type) {
 
-        Map<String, String> error = qrCodeService.errorHandler(size, type);
+        Map<String, String> error = qrCodeService.errorHandler(contents, size, type);
         if (!error.isEmpty())
             return ResponseEntity.badRequest().body(error);
 
         MediaType imageType = qrCodeService.getMediaType(type);
 
-        BufferedImage bufferedImage = qrCodeService.createImage(size);
+        BufferedImage bufferedImage = qrCodeService.createImage(contents, size);
         return ResponseEntity.ok().contentType(imageType).body(bufferedImage);
     }
 }
--- QRCode Service/task/src/qrcodeapi/service/QrCodeService.java
+++ QRCode Service/task/src/qrcodeapi/service/QrCodeService.java
@@ -1,9 +1,13 @@
 package qrcodeapi.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -11,21 +15,24 @@ import java.util.Map;
 
 @Service
 public class QrCodeService {
-    public BufferedImage createImage(int size) {
-        final int ZERO = 0;
-        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = image.createGraphics();
-
-        g.setColor(Color.white);
-        g.fillRect(ZERO, ZERO, size, size);
-
-        g.dispose();
-        return image;
+    public BufferedImage createImage(String content, int size) {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, size, size);
+            return MatrixToImageWriter.toBufferedImage(bitMatrix);
+        } catch (WriterException e) {
+            System.out.println("Error: " + e);
+        }
+        return null;
     }
 
-    public Map<String, String> errorHandler(int size, String type) {
+    public Map<String, String> errorHandler(String contents, int size, String type) {
         Map<String, String> error = new HashMap<>();
         String[] possibleTypes = {"png", "jpeg", "gif"};
+        if (contents.trim().isEmpty()) {
+            error.put("error", "Contents cannot be null or blank");
+            return error;
+        }
         if (size > 350 || size < 150) {
             error.put("error", "Image size must be between 150 and 350 pixels");
             return error;
