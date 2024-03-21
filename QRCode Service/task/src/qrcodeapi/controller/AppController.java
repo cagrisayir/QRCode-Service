05920208ccce25a6976cfd6b34bed3b7d9496694
cagrisayir <omercagris@gmail.com>
2024-03-21 13:20:44 +0300

    qr code endpoint now can send image

--- QRCode Service/task/src/qrcodeapi/controller/AppController.java
+++ QRCode Service/task/src/qrcodeapi/controller/AppController.java
@@ -1,9 +1,13 @@
 package qrcodeapi.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import qrcodeapi.ImageCreator;
+
+import java.awt.image.BufferedImage;
 
 @RestController
 public class AppController {
@@ -14,7 +18,8 @@ public class AppController {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<HttpStatus> qrcode() {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<BufferedImage> getImage() {
+        BufferedImage bufferedImage = new ImageCreator().create250();
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(bufferedImage);
     }
 }
